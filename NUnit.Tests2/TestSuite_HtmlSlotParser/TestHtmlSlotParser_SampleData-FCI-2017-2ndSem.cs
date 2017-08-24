599693ea4fa515e8710365320caa90509747bd5b
JY-HEE <heejiayi@gmail.com>
2017-08-24 13:28:16 +0800

    Added Data From No1 to No2
    for TestHtmlSlotParser_SampleData_FCI_2017_2ndSem.cs

--- NUnit.Tests2/TestSuite_HtmlSlotParser/TestHtmlSlotParser_SampleData-FCI-2017-2ndSem.cs
+++ NUnit.Tests2/TestSuite_HtmlSlotParser/TestHtmlSlotParser_SampleData-FCI-2017-2ndSem.cs
@@ -9,8 +9,10 @@ namespace NUnit.Tests2.TestSuite_HtmlSlotParser {
         protected override List<Slot> GenerateExpectedSlots() {
             return new List<Slot>()
             {
-				new Slot(1, "MPU3113", "HUBUNGAN ETNIK (FOR LOCAL STUDENTS)".Beautify(), "1", "L", Day.Monday, "KB521",new TimePeriod(Time.CreateTime_24HourFormat(9, 00), Time.CreateTime_24HourFormat(12, 00)),new WeekNumber(new List<int>() {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14})),
-            };
+				new Slot(1, "MPU3113", "HUBUNGAN ETNIK (FOR LOCAL STUDENTS)".Beautify(), "1", "L", Day.Monday, "KB521",new TimePeriod(Time.CreateTime_24HourFormat(9, 00), Time.CreateTime_24HourFormat(12, 00)),new WeekNumber(new List<int>() {1, 2, 3, 4, 5, 6, 7})),
+            			new Slot(1, "MPU3113", "HUBUNGAN ETNIK (FOR LOCAL STUDENTS)".Beautify(), "1", "L", Day.Thursday, "KB304",new TimePeriod(Time.CreateTime_24HourFormat(8, 00), Time.CreateTime_24HourFormat(11, 00)),new WeekNumber(new List<int>() {1, 2, 3, 4, 5, 6, 7})),
+		    		new Slot(2, "MPU3123", "TAMADUN ISLAM DAN TAMADUN ASIA (TITAS)".Beautify(), "2", "L", Day.Tuesday, "KB110",new TimePeriod(Time.CreateTime_24HourFormat(12, 00), Time.CreateTime_24HourFormat(15, 00)),new WeekNumber(new List<int>() {1, 2, 3, 4, 5, 6, 7})),
+	    };
         }
     }
 }
